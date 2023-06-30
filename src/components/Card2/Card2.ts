1e2a7240f6eb387855af3d884312818d80892317
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-06-30 18:39:03 +0200

    1

--- src/components/Card2/Card2.ts
+++ src/components/Card2/Card2.ts
@@ -1,13 +1,15 @@
 import { defineComponent, ref } from 'vue'
 import leftPic from '@/assets/pictures/home-l.png'
 import rightPic from '@/assets/pictures/home-r.png'
+import lr from '@/assets/pictures/lr.png'
 
 export default defineComponent({
   name: 'Hero',
   setup() {
     return {
       leftPic,
-      rightPic
+      rightPic,
+      lr
     }
   }
 })
--- src/components/Card2/Card2.vue
+++ src/components/Card2/Card2.vue
@@ -8,37 +8,34 @@
         <span class="font-black">desires</span>, I deliver a tailored product experience.
       </p>
     </div>
-    <div class="flex h-[80%]">
+    <div class="flex">
       <div class="lr-container">
         <div v-motion-slide-visible-once-left :delay="300" class="l-container">
-          <div class="flex justify-center text-center w-96 p-12 sm-max:hidden">
+          <div class="flex justify-center text-center w-96 p-12 lg-max:hidden">
             <p class="text-container text-right">
               Learning the user needs and desires, I deliver a tailored product experience.
             </p>
           </div>
-          <img class="img-size mb-12" :src="leftPic" />
-          <div
-            class="bg-pink-500 opacity-20 rounded-full w-80 h-96 -z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-3xl"
-          />
         </div>
+      </div>
+      <div v-motion-fade-visible-once :delay="900">
+        <div
+          class="bg-pink-500 opacity-50 rounded-full w-64 h-64 -z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-3xl"
+        ></div>
+        <img :src="lr" alt="" />
       </div>
       <div class="lr-container">
         <div v-motion-slide-visible-once-right :delay="900" class="r-container">
-          <img class="img-size mt-12" :src="rightPic" />
-          <div class="flex justify-center items text-center w-96 p-12 sm-max:hidden">
-            <p class="text-container text-left">
+          <div class="flex justify-center items-center text-center w-96 p-12 lg-max:hidden">
+            <p class="text-container text-left ">
               A product that integrates both user experience and interface design.
             </p>
           </div>
-
-          <div
-            class="bg-teal-700 opacity-20 w-96 h-96 -z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-3xl"
-          />
         </div>
       </div>
     </div>
     <div class="card2-text">
-      <p>
+      <p class="">
         A product that integrates both user <span class="font-black">experience</span> and interface
         <span class="font-black">design</span>.
       </p>
@@ -48,27 +45,34 @@
 
 <style>
 .card2-container {
-  @apply flex flex-col  h-screen overflow-hidden;
+  @apply flex flex-col justify-center min-h-screen;
 }
 .lr-container {
   @apply w-full flex items-center relative;
 }
 .text-container {
-  @apply text-white font-sofia text-xl sm-max:text-sm;
+  @apply text-white font-sofia text-xl;
 }
 .card2-text {
-  @apply text-white h-[10%] flex items-center justify-center text-center font-extralight font-sofia px-4 sm-max:px-8 sm:hidden;
+  @apply text-white h-16 flex items-center justify-center text-center font-extralight font-sofia px-4  sm-max:px-8 lg:hidden;
 }
 .l-container {
-  @apply absolute -right-10 flex items-center;
+  @apply flex-grow flex items-center justify-end;
 }
 .r-container {
-  @apply absolute -left-10  flex items-center;
+  @apply flex-grow flex items-center justify-start;
 }
 .img-size {
-  @apply h-[648px] sm-max:h-[450px];
+  @apply max-h-[80vh] sm-max:h-[450px];
 }
 .animate-spin {
   animation: spin 100s linear infinite;
 }
 </style>
+
+<!-- .l-container {
+  @apply absolute -right-10 flex items-center;
+}
+.r-container {
+  @apply absolute -left-10  flex items-center;
+} -->
