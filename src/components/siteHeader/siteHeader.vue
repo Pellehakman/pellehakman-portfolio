4ff3ee3e52ca9cbf1411bd367ffe83377f480b15
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-06-30 19:49:44 +0200

    Update siteHeader.vue

--- src/components/siteHeader/siteHeader.vue
+++ src/components/siteHeader/siteHeader.vue
@@ -13,13 +13,9 @@
           product they need. All desgins below are made by me.
         </h2>
         <div
-          class="flex justify-end items-end w-full sm-max:justify-center h-full sm-max:items-center relative"
+          class="flex justify-end items-end w-full sm-max:justify-center h-full sm-max:items-center"
         >
-          <div
-            class="text-white font-bold rounded-sm shadow-md whitespace-nowrap transition-all absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          >
-            <p>Scroll down to learn more</p>
-          </div>
+          <p class="text-white font-bold rounded-sm shadow-md mx-4">Scroll down to learn more</p>
         </div>
       </section>
     </div>
